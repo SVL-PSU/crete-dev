8964088100b9b5c6e04d2ba556c9a21cb8255797
Bo Chen <chenbo@pdx.edu>
2017-03-23 18:09:58 -0700

    Add crete-tc-compare

---
+++ misc/util/tc-compare/tc-compare.hpp
@@ -0,0 +1,43 @@
+#ifndef CRETE_REPLAY_H
+#define CRETE_REPLAY_H
+
+#include <crete/test_case.h>
+#include <crete/harness_config.h>
+
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/options_description.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace crete
+{
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
+
+class CreteTcCompare
+{
+private:
+    po::options_description m_ops_descr;
+    po::variables_map m_var_map;
+
+    fs::path m_cwd;
+    fs::path m_ref;
+    fs::path m_tgt;
+
+    bool m_tc_folder;
+
+public:
+    CreteTcCompare(int argc, char* argv[]);
+
+private:
+    boost::program_options::options_description make_options();
+    void process_options(int argc, char* argv[]);
+
+    void compare_tc();
+};
+
+}
+
+#endif // CRETE_REPLAY_H
