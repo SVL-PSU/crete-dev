5dc8f30cf7305d653edfb26ddf46ea2a6d402dde
Bo Chen <chenbo@pdx.edu>
2017-04-20 09:05:25 -0700

    Update script for replaying baseTool experiments

--- misc/scripts/replay/br_basetools.sh
+++ misc/scripts/replay/br_basetools.sh
@@ -2,13 +2,13 @@
 # editing your .lcovrc file (copied from /etc/lcovrc) to change lcov_branch_coverage setting to 1
 
 HOST_CRETE_BIN_DIR="/home/chenbo/crete/build/bin/"
-HOST_PROG_DIR="/home/chenbo/crete/replay-evals/tianocore-baseTools/Source/C/bin/"
+HOST_PROG_DIR="/home/chenbo/crete/replay-evals/edk2/BaseTools/Source/C/bin/"
 
 CRETE_BIN_DIR=$HOST_CRETE_BIN_DIR
 PARSEGCOVCMD=$CRETE_BIN_DIR/../../crete-dev/misc/scripts/replay/parse_gcov_coreutils.py
 PROG_DIR=$HOST_PROG_DIR
 LCOV_DIR=$HOST_PROG_DIR/../
-SANDBOX="/home/chenbo/crete/replay-evals/sandbox"
+# SANDBOX="/home/chenbo/crete/replay-evals/sandbox"
 
 # GUEST_CRETE_BIN_DIR="/home/test/guest-build/bin"
 # GUEST_PROG_DIR="/home/test/tests/tianocore-baseTools/Source/C/bin/"
