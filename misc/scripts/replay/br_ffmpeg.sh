382d4a1d328a3ec6efdd7fd8c3fc1dac89557508
Bo Chen <chenbo@pdx.edu>
2017-05-22 12:03:51 -0700

    Update replay script for ffmpeg

--- misc/scripts/replay/br_ffmpeg.sh
+++ misc/scripts/replay/br_ffmpeg.sh
@@ -1,12 +1,16 @@
 # To generate branch coverage:
 # editing your .lcovrc file (copied from /etc/lcovrc) to change lcov_branch_coverage setting to 1
 HOST_CRETE_BIN_DIR="/home/chenbo/crete/build/bin/"
-HOST_PROG_DIR="/home/chenbo/crete/replay-evals/ffmpeg-3.1.2/"
+HOST_PROG_DIR="/home/chenbo/crete/replay-evals/ffmpeg-3.3.1/"
 
 CRETE_BIN_DIR=$HOST_CRETE_BIN_DIR
 PARSEGCOVCMD=$CRETE_BIN_DIR/../../crete-dev/misc/scripts/replay/parse_gcov_coreutils.py
 PROG_DIR=$HOST_PROG_DIR
-LCOV_DIR=$HOST_PROG_DIR/
+LCOV_DIR=$HOST_PROG_DIR
+# UNIFIED_ENV="/home/chenbo/crete/crete-dev/front-end/guest/sandbox/env/klee-test.env"
+
+# CHECK_EXPLOITABLE_SCRIPT="/home/chenbo/crete/crete-dev/misc/util/tc-replay/check-exploitable/exploitable/exploitable.py"
+# LAUNCH_DIR="/home/chenbo/crete/replay-evals/sandbox"
 # SANDBOX="/home/chenbo/crete/replay-evals/sandbox"
 
 # GUEST_CRETE_BIN_DIR="/home/test/guest-build/bin"
