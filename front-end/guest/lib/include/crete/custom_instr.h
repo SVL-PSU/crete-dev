d47cd4086eaeee8c3483ddc589323bf4fee4c4c3
Bo Chen <chenbo@pdx.edu>
2017-10-30 14:07:36 -0700

    Cleanup crete custom opcode

--- front-end/guest/lib/include/crete/custom_instr.h
+++ front-end/guest/lib/include/crete/custom_instr.h
@@ -12,31 +12,23 @@
 extern "C" {
 #endif // __cplusplus
 
+// For crete-run/preload-run
+void crete_insert_instr_read_port(uintptr_t addr, uintptr_t size);
+void crete_send_custom_instr_prime();
 void crete_send_target_pid(void);
 void crete_void_target_pid(void);
-void crete_insert_instr_addr_exclude_filter(uintptr_t addr_begin, uintptr_t addr_end);
-void crete_insert_instr_addr_include_filter(uintptr_t addr_begin, uintptr_t addr_end);
-void crete_insert_instr_call_stack_exclude(uintptr_t addr_begin, uintptr_t addr_end);
-void crete_insert_instr_call_stack_size(uintptr_t size);
-void crete_insert_instr_stack_depth_bounds(intptr_t low, intptr_t high);
-void crete_insert_instr_main_address(uintptr_t addr, uintptr_t size);
-void crete_insert_instr_libc_exit_address(uintptr_t addr, uintptr_t size);
-void crete_insert_instr_libc_start_main_address(uintptr_t addr, uintptr_t size);
-void crete_send_function_entry(uintptr_t addr, uintptr_t size, const char* name);
-void crete_send_custom_instr_quit();
 void crete_send_custom_instr_dump();
-void crete_send_custom_instr_prime();
-void crete_send_custom_instr_start_stopwatch();
-void crete_send_custom_instr_stop_stopwatch();
-void crete_send_custom_instr_reset_stopwatch();
-void crete_insert_instr_next_replay_program(uintptr_t addr, uintptr_t size);
-void crete_insert_instr_read_port(uintptr_t addr, uintptr_t size);
 
+// For program under test
 void crete_make_concolic(void* addr, size_t size, const char* name);
-
 #define crete_assume(cond)                      \
   crete_assume_begin();                         \
   crete_assume_(cond)
+
+// unused, being kept for future usage
+void crete_insert_instr_addr_exclude_filter(uintptr_t addr_begin, uintptr_t addr_end);
+void crete_insert_instr_addr_include_filter(uintptr_t addr_begin, uintptr_t addr_end);
+void crete_send_custom_instr_quit();
 
 #ifdef __cplusplus
 }
