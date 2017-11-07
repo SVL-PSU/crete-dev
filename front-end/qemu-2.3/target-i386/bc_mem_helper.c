e6e4856514aeb938a9eb036644bc71d441d1267c
Bo Chen <chenbo@pdx.edu>
2017-11-06 16:55:36 -0800

    Add helper_cmpxchg8b() to crete_bc_target_helpers.bc

--- front-end/qemu-2.3/target-i386/bc_mem_helper.c
+++ front-end/qemu-2.3/target-i386/bc_mem_helper.c
@@ -1,3 +1,5 @@
+#include "cpu.h"
+
 void helper_lock(void)
 {
   ;
@@ -6,4 +8,24 @@ void helper_lock(void)
 void helper_unlock(void)
 {
   ;
+}
+
+void helper_cmpxchg8b(CPUX86State *env, target_ulong a0)
+{
+    uint64_t d;
+    int eflags;
+
+    eflags = cpu_cc_compute_all(env, CC_OP);
+    d = cpu_ldq_data(env, a0);
+    if (d == (((uint64_t)env->regs[R_EDX] << 32) | (uint32_t)env->regs[R_EAX])) {
+        cpu_stq_data(env, a0, ((uint64_t)env->regs[R_ECX] << 32) | (uint32_t)env->regs[R_EBX]);
+        eflags |= CC_Z;
+    } else {
+        /* always do the store */
+        cpu_stq_data(env, a0, d);
+        env->regs[R_EDX] = (uint32_t)(d >> 32);
+        env->regs[R_EAX] = (uint32_t)d;
+        eflags &= ~CC_Z;
+    }
+    CC_SRC = eflags;
 }
