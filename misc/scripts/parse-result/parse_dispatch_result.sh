611cf2d2945347a5f21d2d356e7ab34c93fafa98
Bo Chen <chenbo@pdx.edu>
2017-11-22 16:17:43 -0800

    Improve script for parsing dispatch result folder
    1. print unusual klee error
    2. check tc/trace number consistency in finish_log.txt
    3. print out duplicated tc number

--- misc/scripts/parse-result/parse_dispatch_result.sh
+++ misc/scripts/parse-result/parse_dispatch_result.sh
@@ -12,11 +12,37 @@ error_summary()
         return
     fi
 
-
     printf "Totoal error: "
     grep -c '\[2017\-' $INPUT_DIR/log/node_error.log
-    printf "SVM error: "
+
+    printf "\nVM error: "
+    grep -c -w 'Node:\sVM' $INPUT_DIR/log/node_error.log
+    printf "getHostAddress() error: "
+    grep -c -w 'getHostAddress\(\)' $INPUT_DIR/log/node_error.log
+    printf "trace-tag-error: "
+    grep -c "crete-qemu-2.3-system-i386:.*void RuntimeEnv::add_trace_tag(const TranslationBlock\*, uint64_t): Assertion .* failed."  < $INPUT_DIR/log/node_error.log
+
+    printf "\nSVM error: "
     grep -c -w 'Node: SVM' $INPUT_DIR/log/node_error.log
+
+    printf "\nKLEE Warning:"
+    grep -c "KLEE: WARNING: " < $INPUT_DIR/log/node_error.log
+
+    printf "KLEE: WARNING: unable to compute initial values: "
+    grep -c "KLEE: WARNING: unable to compute initial values" < $INPUT_DIR/log/node_error.log
+    printf "KLEE: WARNING: max-intruction-time execeeded: "
+    grep -c "KLEE: WARNING: max-instruction-time exceeded" < $INPUT_DIR/log/node_error.log
+    printf "KLEE: WARNING: STP timed out: "
+    grep -c "KLEE: WARNING: STP timed out" < $INPUT_DIR/log/node_error.log
+
+    printf "\nOther KLEE prints:\n"
+    printf 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n'
+    grep "KLEE" $INPUT_DIR/log/node_error.log \
+        | grep -v "KLEE: output directory is " | grep -v "KLEE: done" \
+        | grep -v "KLEE: WARNING: unable to compute initial values" \
+        | grep -v "KLEE: WARNING: max-instruction-time exceeded"  \
+        | grep -v "KLEE: WARNING: STP timed out"
+    printf '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n\n'
 
     printf "cross-check failed: "
     grep -c "QemuRuntimeInfo::cross_check_cpuState.*failed." < $INPUT_DIR/log/node_error.log
@@ -42,8 +68,6 @@ error_summary()
     grep -c "debug_regs" < $INPUT_DIR/log/node_error.log
 
     printf "\nError Reported by KLEE:\n"
-    printf "STP timed out: "
-    grep -c "STP timed out" < $INPUT_DIR/log/node_error.log
     printf "HaltTimer: "
     grep -c "KLEE: HaltTimer invoked" < $INPUT_DIR/log/node_error.log
     printf "max-instruction-time exceeded: "
@@ -56,13 +80,6 @@ error_summary()
     grep -c "Invalid test case" < $INPUT_DIR/log/node_error.log
     printf "Trace-tag: forking not from captured bitcode: "
     grep -c "klee: Executor.cpp.*Executor::StatePair klee::Executor::crete_concolic_fork(klee::ExecutionState &, ref<klee::Expr>).*\[CRETE FIXME\] klee forks not from captured bitcode.* failed." < $INPUT_DIR/log/node_error.log
-
-    printf "\nVM error: "
-    grep -c -w 'Node:\sVM' $INPUT_DIR/log/node_error.log
-    printf "getHostAddress() error: "
-    grep -c -w 'getHostAddress\(\)' $INPUT_DIR/log/node_error.log
-    printf "trace-tag-error: "
-    grep -c "crete-qemu-2.3-system-i386:.*void RuntimeEnv::add_trace_tag(const TranslationBlock\*, uint64_t): Assertion .* failed."  < $INPUT_DIR/log/node_error.log
 }
 
 error_detail()
@@ -115,6 +132,44 @@ finish_log()
     done
 }
 
+duplicated_tc()
+{
+    printf "\n"
+    printf "++++++++++++++++++++++++++++++++++++++++++++++\n"
+    printf "Duplicated Test case\n"
+    printf "++++++++++++++++++++++++++++++++++++++++++++++\n"
+
+
+    for folder in "${folder_list[@]}"
+    do
+        num_total_tc=$(tail -1 $INPUT_DIR/$folder/log/finish.log | grep -P '\d+' -o | awk 'NR==3')
+        num_tc_pool=$(tail -1 $INPUT_DIR/$folder/log/finish.log | grep -P '\d+' -o | awk 'NR==2')
+        num_tc_vm=$(tail -1 $INPUT_DIR/$folder/log/finish.log | grep -P '\d+' -o | awk 'NR==6')
+        num_dup_tc=$(awk '{print($6)}' $INPUT_DIR/$folder/log/test_case_tree.log)
+        num_issued_tc=$((((num_total_tc-num_tc_pool)-num_tc_vm)-num_dup_tc))
+
+        num_trace_pool=$(tail -1 $INPUT_DIR/$folder/log/finish.log | grep -P '\d+' -o | awk 'NR==5')
+        num_trace_vm=$(tail -1 $INPUT_DIR/$folder/log/finish.log | grep -P '\d+' -o | awk 'NR==7')
+        num_trace_total=$((num_trace_pool+num_trace_vm))
+
+        num_vm_error=$(grep -A 10 "2017.*$folder" $INPUT_DIR/log/node_error.log | grep -c -w 'Node:\sVM')
+
+        # 1. check trace/tc number consistency
+        # num_issued_tc == num_trace_total + num_vm_error
+        should_be_zero_or_one=$(((num_issued_tc-num_trace_total)-num_vm_error))
+
+        if [[ "$should_be_zero_or_one" -ne "0" ]] && [[ "$should_be_zero_or_one" -ne "1" ]]; then
+            printf "%s: inconsistent tc/trace number ($should_be_zero_or_one)\n" $folder
+        fi
+
+        # 2. duplicated tc number
+        if [[ "num_dup_tc" -ne "0" ]]; then
+            failure_percent=$((100*$num_dup_tc/$num_issued_tc)); \
+            printf "%s duplicated tc: $num_dup_tc/$num_issued_tc ($failure_percent%%)\n" $folder
+        fi
+
+    done
+}
 
 init_folder_list()
 {
@@ -135,6 +190,10 @@ init_folder_list()
     SUB_FOLDERS=$INPUT_DIR/*.xml
     for f in $SUB_FOLDERS
     do
+        if [ ! -d $f ]; then
+            continue
+        fi
+
         # only get the name of subfolders
         input=$f
         while [[ $input == *'/'* ]]; do
@@ -155,6 +214,7 @@ main()
 {
     init_folder_list
     error_summary
+    duplicated_tc
     error_detail
     last_tc_time
     finish_log
