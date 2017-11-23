cde43dc021a2ce793d96c08b1b3ce2d932da8bc5
Bo Chen <chenbo@pdx.edu>
2017-11-22 16:55:35 -0800

    Improve guest building process

--- front-end/guest/evals/build-evals.sh
+++ front-end/guest/evals/build-evals.sh
@@ -11,23 +11,27 @@ mkdir coreutils-6.10/build
 cd coreutils-6.10/build
 ../configure --disable-nls  --prefix=$work_dir/build
 make
+make -C src arch hostname
 make install
+cp src/hostname $work_dir/build/bin
+cp src/ginstall $work_dir/build/bin
+cp src/setuidgid $work_dir/build/bin
 
-cd $work_dir
-tar xf $1/diffutils-3.3.tar.xz
-mkdir diffutils-3.3/build
-cd diffutils-3.3/build
-../configure --disable-nls  --prefix=$work_dir/build
-make
-make install
+# cd $work_dir
+# tar xf $1/diffutils-3.3.tar.xz
+# mkdir diffutils-3.3/build
+# cd diffutils-3.3/build
+# ../configure --disable-nls  --prefix=$work_dir/build
+# make
+# make install
 
-cd $work_dir
-tar xf $1/grep-2.18.tar.xz
-mkdir grep-2.18/build
-cd grep-2.18/build
-../configure --disable-nls  --prefix=$work_dir/build
-make
-make install
+# cd $work_dir
+# tar xf $1/grep-2.18.tar.xz
+# mkdir grep-2.18/build
+# cd grep-2.18/build
+# ../configure --disable-nls  --prefix=$work_dir/build
+# make
+# make install
 
 
 ## for coverage setup
